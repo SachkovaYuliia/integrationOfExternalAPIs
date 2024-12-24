29b0bf761226b982a2fc0f906eec774540567d59
sachkova_yuliia <sachkova_yuliia@gmail.com>
2024-12-24 19:41:23 +0100

    logs added

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ import requests
 from http.client import responses
 from dotenv import load_dotenv
 import os
+import logging
+import time
 
 load_dotenv()
 
 api_key = os.getenv('API_KEY')
 api_token = os.getenv('TG_TOKEN')
+
+logging.basicConfig(
+    filename='app.log', 
+    level=logging.DEBUG,  
+    format='%(asctime)s - %(levelname)s - %(message)s',  
+)
+
+start_time = time.time()
+elapsed_time = time.time() - start_time
 
 city = input("Введіть місто: ")
 url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
